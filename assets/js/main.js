f26f3d62083e5538afa6d16b8173d49336ed8c9f
Shubham Arora <shubham.arora4538@gmail.com>
2016-09-10 21:59:52 +0530

    cloned files from previous account

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+if ('addEventListener' in window) {
+    window.addEventListener('load', function () {
+        document.body.className = document.body.className.replace(/\bloading\b/, '');
+    });
+    document.body.className += (navigator.userAgent.match(/(MSIE|rv:11\.0)/) ? ' is-ie' : '');
+    $(".social").addClass("animated zoomIn");
+    $("#resume").addClass("animated slideInUp");
+    $(".avatar").addClass("animated tada");
+    $("#hi").addClass("animated zoomInUp");
+    $("#main").addClass("animated flipInY");
+}
+
+document.getElementById("res121").disabled = true;
+$("#res_a").addClass("not-active");
+
+ function enableBtn(){
+    document.getElementById("res121").disabled = false;
+     $("#res_a").removeClass("not-active");
+
+   }
